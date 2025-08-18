08f64fa1d909966759ba217c841b6d5b87874f82
Owais Idrisi <idrisiowais05@gmail.com>
2025-08-18 15:48:04 +0530

    write code to Create public/temp directory if it does not exist for production

--- server/index.js
+++ server/index.js
@@ -1,5 +1,17 @@
 import { app } from "./app.js";
 import { connectToDB } from "./db/index.js";
+import fs from "fs";
+import path from "path";
+
+// folder path
+const tempDir = path.join(process.cwd(), "public/temp");
+
+// check & create folder if not exists
+if (!fs.existsSync(tempDir)) {
+    fs.mkdirSync(tempDir, { recursive: true });
+    console.log("✅ public/temp folder created");
+}
+
 
 connectToDB()
     .then(() => {
